31a7f800832d3aae2146bf372a672c7a94a83e70
M.M.Hasibuzzaman <h.zzzaman@gmail.com>
2020-04-15 17:13:49 +0600

    First commit

---
+++ lib/screens/home/authenticate/register.dart
@@ -0,0 +1,108 @@
+import 'package:brewcrew/services/auth.dart';
+import 'package:brewcrew/shared/constant.dart';
+import 'package:brewcrew/shared/loading.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+
+  final toggleView;
+  Register({this.toggleView});
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
+  //text field state value
+  String email = '';
+  String password = '';
+  String error = '';
+  bool loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        title: Text(
+          'Sign up for Brew Crew',
+        ),
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed: (){
+              //GOES TO SIGN IN
+              widget.toggleView();
+            },
+            icon: Icon(Icons.person),
+            label: Text('Sign in'),
+          )
+        ],
+        backgroundColor: Colors.brown[300],
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20,horizontal: 50),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0,),
+              //EMAIL FIELD
+              TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                validator: (val) => val.isEmpty ? 'Email Field Empty' : null,
+                onChanged: (val){
+                  setState(() => email = val);
+                },
+              ),
+              SizedBox(height: 20.0,),
+              //PASSWORD FIELD
+              TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                validator: (val) => val.length < 6 ? 'Enter password 6+ char long' : null,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+                obscureText: true,
+              ),
+              SizedBox(height: 10,),
+              loading ? LoadingSpinKit() : RaisedButton(
+                onPressed: () async{
+                  if(_formKey.currentState.validate()){
+                    setState(() => loading = true);
+                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                    if(result == null){
+                      setState(() {
+                        error = 'Please enter a valid Email';
+                        loading = false;
+                      });
+                    }
+                  }
+                },
+                color: Colors.pinkAccent,
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 12.0,),
+              Text(
+                error,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/authenticate/sign_in.dart
@@ -0,0 +1,113 @@
+import 'package:brewcrew/services/auth.dart';
+import 'package:brewcrew/shared/constant.dart';
+import 'package:brewcrew/shared/loading.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+
+  final toggleView;
+  SignIn({this.toggleView});
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  final AuthService _auth = AuthService();
+  final _formState = GlobalKey<FormState>();
+  bool loading = false;
+
+  //text field state value
+  String email = '';
+  String password = '';
+  String error = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        title: Text(
+          'Sign In',
+          style: TextStyle(
+            letterSpacing: 1.5,
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed: (){
+              //GOES TO REGISTER
+              widget.toggleView();
+            },
+            label: Text('Register'),
+            icon: Icon(Icons.person),
+          )
+        ],
+        backgroundColor: Colors.brown[300],
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20,horizontal: 50),
+        child: Form(
+          key: _formState,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20.0,),
+              //EMAIL FIELD
+              TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                validator: (val) => val.isEmpty ? 'Field empty' : null,
+                onChanged: (val){
+                  setState(() => email = val);
+                },
+              ),
+              SizedBox(height: 20.0,),
+              //PASSWORD FIELD
+              TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                validator: (val) => val.length <6 ? 'Password must be 6+ char long' : null,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+                obscureText: true,
+              ),
+              SizedBox(height: 10,),
+              loading ? LoadingSpinKit() : RaisedButton(
+                  onPressed: () async{
+                    if(_formState.currentState.validate()){
+                      setState(() => loading = true);
+                      dynamic result = await _auth.singInWithEmailAndPassword(email, password);
+                      if(result == null){
+                        setState(() {
+                          error = 'Incorrect Email & Password';
+                          loading = false;
+                        });
+                      }
+                    }
+                  },
+              color: Colors.pinkAccent,
+              child: Text(
+                'SignIn',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              ),
+              SizedBox(height: 20,),
+              Text(
+                error,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.red,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
